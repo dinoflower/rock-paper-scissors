8470356730ecc16a13c4c0a7df6079c0675f576d
dinoflower <kaedilove@gmail.com>
2022-03-03 17:20:21 -0600

    return string values rock paper or scissors for 1 2 or 3

--- javascript.js
+++ javascript.js
@@ -1,6 +1,28 @@
 // upon input (in console)
-function computerPlay(){
-    return Math.floor(Math.random()*3) + 1;
-}
+// function computerPlay(){
+//     return Math.floor(Math.random()*3) + 1;
+// }
 
 // return "rock" for 1, "paper" for 2, "scissors" for 3
+function computerPlay(){
+    if (randomNumber() == 1) {
+    result = "rock";
+    } else if (randomNumber() == 2) {
+    result = "paper";
+    } else {
+    result = "scissors";
+    }
+    return result;
+}
+function randomNumber(){
+    return Math.floor(Math.random()*3) + 1;
+}
+//    if (result == 1){
+//    return "rock";
+// }
+// else-if (result == 2){
+//     return "paper";
+// }
+// else {
+//    return "scissors";
+// }
