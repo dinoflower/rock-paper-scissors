80681b4175a7f8ac7342665d09d77c23bd3b19c3
dinoflower <kaedilove@gmail.com>
2022-04-10 18:35:49 -0500

    update playRound()

--- javascript.js
+++ javascript.js
@@ -4,57 +4,62 @@
 
 let playerSelection = "rock";
 
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay(array){
     let rand = Math.random()*array.length | 0;
     var computerSelection = array[rand];
     return computerSelection;
 }
 var computerChoices = ["rock", "paper", "scissors"];
-var computerSelection = computerPlay(computerChoices);
+//var computerSelection = computerPlay(computerChoices);
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "rock") {
-        roundOutcome = rockTie;
+    let computerSelection = computerPlay(computerChoices);
+    if (playerSelection == computerSelection) {
+        if (playerSelection == "rock") {
+            return "It's a tie! Did you think rock was weak to rock?";
+        }
+        else if (playerSelection == "paper") {
+            return "It's a tie! The papers harmlessly lie on a desk.";
+        }
+        else {
+            return "It's a tie! These scissors are evenly matched in skill.";
+        }
     }
     else if (playerSelection == "rock" && computerSelection == "paper") {
-        roundOutcome = rockLoss;
+        computerScore++;
+        return "You lose somehow! Paper covers rock.";
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
-        roundOutcome = rockWin;
+        playerScore++;
+        return "You win! Rock smashes scissors. What a waste of office equipment!";
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        roundOutcome = paperWin;
-    }
-    else if (playerSelection == "paper" && computerSelection == "paper") {
-        roundOutcome = paperTie;
+        playerScore++;
+        return "You win somehow! Paper covers rock.";
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
-        roundOutcome = paperLoss;
+        computerScore++;
+        return "You lose! Scissors cut paper. What were you thinking?"
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
-        roundOutcome = scissorsLoss;
+        computerScore++;
+        return "You lose! Rock smashes scissors. What a waste of office equipment!";
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        roundOutcome = scissorsWin;
-    }
-    else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        roundOutcome = scissorsTie;
+        playerScore++;
+        return "You lose! Scissors cut paper. What was the computer thinking?";
     }
     else {
-        roundOutcome = "Please choose rock, paper, or scissors."
+        return "Please choose rock, paper, or scissors.";
     }
-    return roundOutcome;
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
-function game(){
+function game() {
     for (let i = 0; i < 5; i++) {
         playRound(playerSelection, computerSelection);
-        playerScore += roundOutcome.playerScore;
-        computerScore += roundOutcome.computerScore;
-        console.log(roundOutcome.text);
     }
     console.log(playerScore);
     console.log(computerScore);
@@ -71,57 +76,3 @@ function game(){
         return gameOutcome;
     }
 }
-
-let rockWin = {
-    playerScore: 1,
-    computerScore: 0,
-    text: "You win! Rock smashes scissors."
-};
-
-let paperWin = {
-    playerScore: 1,
-    computerScore: 0,
-    text: "You win somehow! Paper covers rock."
-};
-
-let scissorsWin = {
-    playerScore: 1,
-    computerScore: 0,
-    text: "You win! Scissors cut paper."
-};
-
-let rockTie = {
-    playerScore: 0,
-    computerScore: 0,
-    text: "It's a tie! Did you think rock was weak to rock?"
-};
-
-let paperTie = {
-    playerScore: 0,
-    computerScore: 0,
-    text: "It's a tie! The papers harmlessly lie on a desk."
-};
-
-let scissorsTie = {
-    playerScore: 0,
-    computerScore: 0,
-    text: "It's a tie! These scissors are evenly matched in skill."
-};
-
-let rockLoss = {
-    playerScore: 0,
-    computerScore: 1,
-    text: "You lose somehow! Paper covers rock."
-};
-
-let paperLoss = {
-    playerScore: 0,
-    computerScore: 1,
-    text: "You lose! Scissors cut paper."
-};
-
-let scissorsLoss = {
-    playerScore: 0,
-    computerScore: 1,
-    text: "You lose! Rock smashes scissors."
-};
