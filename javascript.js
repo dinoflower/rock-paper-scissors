0ae80c89005da3f458ec33de889bf09a08deb0bd
dinoflower <kaedilove@gmail.com>
2022-05-04 13:48:19 -0500

    buttons now remove old p and display new

--- javascript.js
+++ javascript.js
@@ -20,19 +20,33 @@ rockButton.addEventListener("click", () => { playRound("rock", computerSelection
 paperButton.addEventListener("click", () => { playRound("paper", computerSelection) });
 scissorsButton.addEventListener("click", () => { playRound("scissors", computerSelection) });
 
+let results = document.querySelector("#results_container");
+let text;
 
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay(computerChoices);
     if (playerSelection === computerSelection) {
         if (playerSelection === "rock") {
+            results.replaceChildren();
+            text = document.createElement('p');
+            text.textContent = "It's a tie! Did you think rock was weak to rock?";
+            results.appendChild(text);
             console.log("It's a tie! Did you think rock was weak to rock?");
             return "It's a tie! Did you think rock was weak to rock?";
         }
         else if (playerSelection === "paper") {
+            results.replaceChildren();
+            text = document.createElement('p');
+            text.textContent = "It's a tie! The papers harmlessly lie on a desk.";
+            results.appendChild(text);
             console.log("It's a tie! The papers harmlessly lie on a desk.");
             return "It's a tie! The papers harmlessly lie on a desk.";
         }
         else if (playerSelection === "scissors") {
+            results.replaceChildren();
+            text = document.createElement('p');
+            text.textContent = "It's a tie! These scissors are evenly matched in skill.";
+            results.appendChild(text);
             console.log("It's a tie! These scissors are evenly matched in skill.");
             return "It's a tie! These scissors are evenly matched in skill.";
         }
@@ -42,36 +56,60 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
         computerScore += 1;
+        results.replaceChildren();
+        text = document.createElement('p');
+        text.textContent = "You lose somehow! Paper covers rock.";
+        results.appendChild(text);
         console.log("You lose somehow! Paper covers rock.");
         return "You lose somehow! Paper covers rock.";
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore += 1;
+        results.replaceChildren();
+        text = document.createElement('p');
+        text.textContent = "You win! Rock smashes scissors. What a waste of office equipment!";
+        results.appendChild(text);
         console.log("You win! Rock smashes scissors. What a waste of office equipment!");
         return "You win! Rock smashes scissors. What a waste of office equipment!";
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore += 1;
+        results.replaceChildren();
+        text = document.createElement('p');
+        text.textContent = "You win somehow! Paper covers rock.";
+        results.appendChild(text);
         console.log("You win somehow! Paper covers rock.");
         return "You win somehow! Paper covers rock.";
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
         computerScore += 1;
+        results.replaceChildren();
+        text = document.createElement('p');
+        text.textContent = "You lose! Scissors cut paper. What were you thinking?";
+        results.appendChild(text);
         console.log("You lose! Scissors cut paper. What were you thinking?");
         return "You lose! Scissors cut paper. What were you thinking?";
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
         computerScore += 1;
+        results.replaceChildren();
+        text = document.createElement('p');
+        text.textContent = "You lose! Rock smashes scissors. What a waste of office equipment!";
+        results.appendChild(text);
         console.log("You lose! Rock smashes scissors. What a waste of office equipment!");
         return "You lose! Rock smashes scissors. What a waste of office equipment!";
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerScore += 1;
-        console.log("You lose! Scissors cut paper. What was the computer thinking?");
-        return "You lose! Scissors cut paper. What was the computer thinking?";
+        results.replaceChildren();
+        text = document.createElement('p');
+        text.textContent = "You win! Scissors cut paper. What was the computer thinking?";
+        results.appendChild(text);
+        console.log("You win! Scissors cut paper. What was the computer thinking?");
+        return "You win! Scissors cut paper. What was the computer thinking?";
     }
     else {
-        console.log("You lose! Scissors cut paper. What was the computer thinking?");
+        console.log("Cheeky. Please choose rock, paper, or scissors.");
         return "Cheeky. Please choose rock, paper, or scissors.";
     }
 }
